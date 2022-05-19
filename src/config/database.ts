a24a8a57808a4568b98ef843b49636bb9587a32a
unknown <adamszuta@vp.pl>
2022-05-19 22:40:43 +0200

    Change

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 export default {
   client: 'mysql2',
   connection: {
-    host: process.env.DB_HOST || '127.0.0.1',
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
